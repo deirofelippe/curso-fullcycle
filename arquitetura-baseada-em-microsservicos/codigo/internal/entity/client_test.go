0d98a8f882b29b1eb0240054123efdc5ce6bf8fc
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-05-17 23:30:20 -0300

    feat: cria entidade account e testes

--- arquitetura-baseada-em-microsservicos/codigo/internal/entity/client_test.go
+++ arquitetura-baseada-em-microsservicos/codigo/internal/entity/client_test.go
@@ -10,7 +10,7 @@ func TestCreateNewClient(t *testing.T) {
 	client, err := NewClient("John Doe", "j@j.com")
 
 	assert.Nil(t, err)
-	assert.NotNil(t, client)
+assert.NotNil(t, client)
 	assert.Equal(t, "John Doe", client.Name)
 	assert.Equal(t, "j@j.com", client.Email)
 }
@@ -36,4 +36,13 @@ func TestUpdateClientWithInvalidArgs(t *testing.T) {
 	err := client.Update("", "j@j.com")
 
 	assert.Error(t, err, "name is required")
+}
+
+func TestAddAccountToClient(t *testing.T){
+	client,_ := NewClient("John Doe","j@j")
+	account := NewAccount(client)
+	err:=client.AddAccount(account)
+
+	assert.Nil(t,err)
+	assert.Equal(t,1,len(client.Accounts))
 }
