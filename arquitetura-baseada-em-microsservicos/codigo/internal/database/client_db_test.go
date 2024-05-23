796d515883950082d9062d557445130ba6d5671e
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-05-23 08:48:18 -0300

    test(fix): cria email na tabela de client

--- arquitetura-baseada-em-microsservicos/codigo/internal/database/client_db_test.go
+++ arquitetura-baseada-em-microsservicos/codigo/internal/database/client_db_test.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"testing"
 
+	"github.com.br/deirofelippe/curso-fullcycle/internal/entity"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -17,7 +19,7 @@ func (s *ClientDBTestSuite) SetupSuite() {
 	db, err := sql.Open("sqlite3", ":memory:")
 	s.Nil(err)
 	s.db = db
-	db.Exec("Create table clients(id varchar(255), name varchar(255), created_at date)")
+	db.Exec("CREATE TABLE clients(id varchar(255), name varchar(255), email varchar(255), created_at date)")
 	s.clientDB = NewClientDB(db)
 }
 
@@ -28,4 +30,26 @@ func (s *ClientDBTestSuite) TearDownSuite() {
 
 func TestClientDBTestSuite(t *testing.T) {
 	suite.Run(t, new(ClientDBTestSuite))
+}
+
+func (s *ClientDBTestSuite) TestGet() {
+	client, _ := entity.NewClient("john", "john@doe")
+	s.clientDB.Save(client)
+
+	clientDB, err := s.clientDB.Get(client.ID)
+	s.Nil(err)
+	s.Equal(client.ID, clientDB.ID)
+	s.Equal(client.Name, clientDB.Name)
+	s.Equal(client.Email, clientDB.Email)
+}
+
+func (s *ClientDBTestSuite) TestSave() {
+	client := &entity.Client{
+		ID:    "1",
+		Name:  "Test",
+		Email: "test@do",
+	}
+
+	err := s.clientDB.Save(client)
+	s.Nil(err)
 }
