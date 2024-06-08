e77595a8f4b34f387b77714fd6c59842ef89a0c8
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-06-08 19:59:51 -0300

    test: cria testes pra register do event dispatcher

--- eda-event-driven-architecture/codigo/pkg/events/event_dispatcher_test.go
+++ eda-event-driven-architecture/codigo/pkg/events/event_dispatcher_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -53,6 +54,29 @@ func (suite *EventDispatcherTestSuite) SetupTest() {
 	}
 	suite.event1 = TestEvent{Name: "test1", Payload: "test1"}
 	suite.event2 = TestEvent{Name: "test2", Payload: "test2"}
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register() {
+	err := suite.eventDispatcher.Register(suite.event1.GetName(), &suite.handler1)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event1.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event1.GetName(), &suite.handler2)
+	suite.Nil(err)
+	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event1.GetName()]))
+
+	assert.Equal(suite.T(), &suite.handler1, suite.eventDispatcher.handlers[suite.event1.GetName()][0])
+	assert.Equal(suite.T(), &suite.handler2, suite.eventDispatcher.handlers[suite.event1.GetName()][1])
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register_WithSameHandler() {
+	err := suite.eventDispatcher.Register(suite.event1.GetName(), &suite.handler1)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event1.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event1.GetName(), &suite.handler1)
+	suite.Equal(ErrHandlerAlreadyRegistered, err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event1.GetName()]))
 }
 
 func (suite *EventDispatcherTestSuite) TestEventDispatcher_Clear() {
