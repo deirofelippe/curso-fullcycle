69c74263fcf53e97988c74e6455426aa1fead9c6
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-06-16 23:00:36 -0300

    feat(main): adiciona o handler de kafka de new transaction

--- eda-event-driven-architecture/codigo/cmd/walletcore/main.go
+++ eda-event-driven-architecture/codigo/cmd/walletcore/main.go
@@ -7,30 +7,40 @@ import (
 	"fmt"
 	"net/http"
 
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/deirofelippe/curso-fullcycle/internal/database"
 	"github.com/deirofelippe/curso-fullcycle/internal/event"
+	"github.com/deirofelippe/curso-fullcycle/internal/event/handler"
 	createaccount "github.com/deirofelippe/curso-fullcycle/internal/usecase/create_account"
 	createclient "github.com/deirofelippe/curso-fullcycle/internal/usecase/create_client"
 	createtransaction "github.com/deirofelippe/curso-fullcycle/internal/usecase/create_transaction"
 	"github.com/deirofelippe/curso-fullcycle/internal/web"
 	"github.com/deirofelippe/curso-fullcycle/internal/web/webserver"
 	"github.com/deirofelippe/curso-fullcycle/pkg/events"
+	"github.com/deirofelippe/curso-fullcycle/pkg/kafka"
 	"github.com/deirofelippe/curso-fullcycle/pkg/uow"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "localhost", "3306", "wallet"))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3306", "wallet"))
 	if err != nil {
 		panic(err)
 	}
 
 	defer db.Close()
 
-	eventDispatcher := events.NewEventDispatcher()
-	transactionCreatedEvent := event.NewTransactionCreated()
+	configMap := ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:29092",
+		"group.id":          "wallet",
+	}
+	kafkaProducer := kafka.NewKafkaProducer(&configMap)
 
-	// eventDispatcher.Register("TransactionCreated", handler)
+	eventDispatcher := events.NewEventDispatcher()
+	handler := handler.NewTransactionCreatedKafkaHandler(kafkaProducer)
+	eventDispatcher.Register("TransactionCreated", handler)
+
+	transactionCreatedEvent := event.NewTransactionCreated()
 
 	clientDb := database.NewClientDB(db)
 	accountDb := database.NewAccountDB(db)
@@ -50,7 +60,7 @@ func main() {
 	createAccountUsecase := createaccount.NewCreateAccountUsecase(accountDb, clientDb)
 	createTransactionUsecase := createtransaction.NewCreateTransactionUsecase(uow, eventDispatcher, transactionCreatedEvent)
 
-	port := 3000
+	port := 8080
 	webserver := webserver.NewWebServer(fmt.Sprintf(":%d", port))
 
 	clientHandler := web.NewWebClientHandler(*createClientUsecase)
