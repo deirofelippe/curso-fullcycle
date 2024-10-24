8fe0a73aeebee6b8dbc59e65783970d2f08a630d
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-10-24 00:40:53 -0300

    modulo kubernetes - adiciona startup, readiness e liveness probe

--- kubernetes/server.go
+++ kubernetes/server.go
@@ -6,7 +6,11 @@ import (
 	"os"
 )
 
+var isHealthy = true
+
 func main() {
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/set-unhealth", SetUnhealth)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8000", nil)
 }
@@ -14,4 +18,21 @@ func main() {
 func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	fmt.Fprintf(w, "Hello World! %s", name)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	if isHealthy {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	} else {
+		w.WriteHeader(500)
+		w.Write([]byte("Is unhealth"))
+	}
+}
+
+func SetUnhealth(w http.ResponseWriter, r *http.Request) {
+	isHealthy = false
+
+	w.WriteHeader(201)
+	w.Write([]byte("Now is unhealth"))
 }
